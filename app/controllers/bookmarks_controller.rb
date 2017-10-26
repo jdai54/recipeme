de150d2216e40ee3844eabd415adaeaaba4a90a2
Jake <jdai54@gmail.com>
2017-10-26 13:38:12 -0700

    Added Pundit but not working properly in views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,16 @@
 class BookmarksController < ApplicationController
+  # before_action :authenticate_user!, except: [:show]
+  before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
+
   def new
     @topic = Topic.find(params[:topic_id])
+    authorize @bookmark
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new
+    authorize @bookmark
     @bookmark.url = params[:bookmark][:url]
     @topic = Topic.find(params[:topic_id])
     @bookmark.topic = @topic
@@ -16,17 +21,19 @@ class BookmarksController < ApplicationController
 
   def edit
     @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
   end
 
   def update
     @bookmark = Bookmark.find(params[:id])
     @bookmark.url = params[:bookmark][:url]
-
+    authorize @bookmark
     @bookmark.save
     redirect_to root_path
   end
 
   def destroy
+    authorize @bookmark
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to root_path
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,14 +1,18 @@
 class TopicsController < ApplicationController
+  # before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
   def index
     @topics = Topic.all
   end
 
   def show
     @topic = Topic.find(params[:id])
+    authorize @topic
   end
 
   def create
     @topic = Topic.new
+    authorize @topic
     @topic.title = params[:topic][:title]
 
     @topic.save
@@ -17,9 +21,11 @@ class TopicsController < ApplicationController
 
   def edit
     @topic = Topic.find(params[:id])
+    authorize @topic
   end
 
   def update
+    authorize @topic
     @topic = Topic.find(params[:id])
     @topic.title = params[:topic][:title]
 
@@ -28,6 +34,7 @@ class TopicsController < ApplicationController
   end
 
   def destroy
+    authorize @topic
     @topic = Topic.find(params[:id])
     @topic.destroy
 
