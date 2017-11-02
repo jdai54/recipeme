2d289915d5a6258838e5fe655ae656bab13fb32f
Jake <jdai54@gmail.com>
2017-11-02 11:59:09 -0700

    Added create and destroy like actions but tests not passing

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,5 +3,28 @@ class LikesController < ApplicationController
     @likes = current_user.likes
   end
 
+  def create
+    bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.build(bookmark: bookmark)
 
+    if like.save
+      flash[:notice] = "Bookmark liked!"
+    else
+      flash[:alert] = "Liking failed!"
+    end
+    redirect_to bookmark.topic
+  end
+
+  def destroy
+    bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
+
+    if like.destroy
+      flash[:notice] = "Bookmark unliked."
+    else
+      flash[:alert] = "Unliking failed!"
+    end
+
+    redirect_to bookmark.topic
+  end
 end
--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -17,4 +17,34 @@ RSpec.describe LikesController, type: :controller do
       expect(assigns(:likes)).to eq([my_like])
     end
   end
+
+  describe "POST create" do
+    it "redirects to the topics show view" do
+      post :create, { bookmark_id: my_bookmark.id }
+      expect(response).to redirect_to([my_topic, topic])
+    end
+
+    it "creates a like for the current user and the selected bookmark" do
+      expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).to be_nil
+
+      post :create, { bookmark_id: my_bookmark.id }
+
+      expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).not_to be_nil
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "redirects to the topics show view" do
+      like = my_user.likes.where(bookmark: my_bookmark).create
+      delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
+      expect(response).to redirect_to([my_topic, topic])
+    end
+
+    it "destroys the like for the current user and post" do
+      like = my_user.like.where(bookmark: my_bookmark).create
+      expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).not_to be_nil
+      delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
+      expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).to be_nil
+    end
+  end
 end
