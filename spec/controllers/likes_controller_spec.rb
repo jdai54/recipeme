7d4fd23ed55da4121b2daf00ea6d3f168e4c381e
Jake <jdai54@gmail.com>
2017-11-03 09:34:04 -0700

    Likes controller tests passing and fixed database

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -6,45 +6,52 @@ RSpec.describe LikesController, type: :controller do
   let(:my_bookmark) { my_topic.bookmarks.create!(url: "www.tasty.com", user: my_user) }
   let(:my_like) { my_bookmark.like.create!(bookmark: my_bookmark, user: my_user) }
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it "assigns [my_like] to @likes" do
-      get :index
-      expect(assigns(:likes)).to eq([my_like])
-    end
+context "logged in user" do
+  before do
+    my_user.confirm
+    sign_in my_user
+    request.env["HTTP_REFERER"] = "where_i_came_from"
   end
+    describe "GET index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
 
-  describe "POST create" do
-    it "redirects to the topics show view" do
-      post :create, { bookmark_id: my_bookmark.id }
-      expect(response).to redirect_to([my_topic, topic])
+      it "assigns [my_like] to @likes" do
+        get :index
+        expect(assigns(:likes)).to eq([my_like])
+      end
     end
 
-    it "creates a like for the current user and the selected bookmark" do
-      expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).to be_nil
+    describe "POST create" do
+      it "redirects to the topics show view" do
+        post :create, { bookmark_id: my_bookmark.id }
+        expect(response).to redirect_to my_topic
+      end
 
-      post :create, { bookmark_id: my_bookmark.id }
+      it "creates a like for the current user and the selected bookmark" do
+        expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).to be_nil
 
-      expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).not_to be_nil
+        post :create, { bookmark_id: my_bookmark.id }
+
+        expect(my_user.likes.find_by_bookmark_id(my_bookmark.id)).not_to be_nil
+      end
     end
-  end
 
-  describe "DELETE destroy" do
-    it "redirects to the topics show view" do
-      like = my_user.likes.where(bookmark: my_bookmark).create
-      delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
-      expect(response).to redirect_to([my_topic, topic])
-    end
+    describe "DELETE destroy" do
+      it "redirects to the topics show view" do
+        like = my_user.likes.where(bookmark: my_bookmark).create
+        delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
+        expect(response).to redirect_to my_topic
+      end
 
-    it "destroys the like for the current user and post" do
-      like = my_user.like.where(bookmark: my_bookmark).create
-      expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).not_to be_nil
-      delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
-      expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).to be_nil
+      it "destroys the like for the current user and post" do
+        like = my_user.likes.where(bookmark: my_bookmark).create
+        expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).not_to be_nil
+        delete :destroy, { bookmark_id: my_bookmark.id, id: like.id }
+        expect( my_user.likes.find_by_bookmark_id(my_bookmark.id) ).to be_nil
+      end
     end
   end
 end
